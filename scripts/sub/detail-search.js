0e2c3b89bdca328b6b95be075f4a08ec376da665
gyeong <ffar140904@gmail.com>
2026-05-14 14:25:01 +0900

    큐레이션js완료

--- scripts/sub/detail-search.js
+++ scripts/sub/detail-search.js
@@ -3,27 +3,14 @@ const formArea = document.querySelector(".form-area");
 const searchSixth =  formArea.querySelector(".search-key06");
 const dropSortby = searchSixth.querySelector(".sort-by");
 const dropFilter = searchSixth.querySelector(".filter-by");
+const searchFirst = formArea.querySelector(".spacing")
+const formtxt = document.querySelectorAll("input.base-form");
+const searchBtn = document.querySelector(".search-btn");
 
 //함수
 
-//ul로 li여닫기
-// dropFilter.addEventListener("click", (e) => {
-//     const clickedLi = e.target;
-//     if(e){
-//         dropFilter.querySelectorAll("li").forEach((li)=>{
-//             li.style.display = "block";
-//         })
-//     }
-//     if (clickedLi.tagName === "LI") {
-//         const currentSelected = dropFilter.querySelector(".selected");
-//         if (currentSelected) {
-//             currentSelected.classList.remove("selected");
-//             dropFilter.querySelectorAll("li").style.display = "none"
-//         };
-//         clickedLi.classList.add("selected");
+searchBtn.addEventListener("click",(e)=>{
+    e.preventDefault(); //새로고침방지
+    if(formtxt.value == ""){ alert("검색어를 입력해주세요") }
 
-//         dropSortby.style.display = "block";
-        
-//         console.log(e, "클릭됨");
-//     }
-// });
+})
